4beca3334e3ac25ef2c0e1853b3f8ac3e52d91e2
FajardoX <michaeljayfajardo10@gmail.com>
2023-07-19 13:02:54 +1000

    Integers

---
+++ src/integers.java
@@ -0,0 +1,26 @@
+public class integers {
+    public static void main(String[] args) {
+//        Write a program which declares two numbers of integer type and assigns them to 233 and 11, then
+//        calculates and displays their sum, difference, product, and quotient.
+//        Is result for quotient correct?
+        int number1 = 233;
+        int number2 = 11;
+
+//        Sum of numbers
+        int sum = number1 + number2;
+
+//        Difference of numbers
+        int difference = number1 - number2;
+
+//        Product of numbers
+        int product = number1 * number2;
+
+//        Quotient of numbers
+        int quotient = number1 / number2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
