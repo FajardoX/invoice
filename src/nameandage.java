69ace421ea0589eec3d8b14c31d189bae5ea89f3
FajardoX <michaeljayfajardo10@gmail.com>
2023-07-19 12:18:46 +1000

    5 students name and age

---
+++ src/nameandage.java
@@ -0,0 +1,27 @@
+public class nameandage {
+//    assign five students names and their ages in variables
+//    print out the names and ages
+    public static void main(String[] args) {
+        String student1Name = "Jonathan";
+        int student1Age = 16;
+
+        String student2Name = "John";
+        int student2Age = 17;
+
+        String student3Name = "Jamie";
+        int student3Age = 18;
+
+        String student4Name = "Jeremiah";
+        int student4Age = 19;
+
+        String student5Name = "Jimothy";
+        int student5Age = 20;
+
+    System.out.println("Five Students and their Ages");
+    System.out.println(student1Name + ": " + student1Age);
+    System.out.println(student2Name + ": " + student2Age);
+    System.out.println(student3Name + ": " + student3Age);
+    System.out.println(student4Name + ": " + student4Age);
+    System.out.println(student5Name + ": " + student5Age);
+    }
+}
